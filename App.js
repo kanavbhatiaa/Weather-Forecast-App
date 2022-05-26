99ceb0fc9448b5f2e24151debe0fe32bdd243a32
kanavbhatiaa <63281984+kanavbhatiaa@users.noreply.github.com>
2022-05-26 07:26:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './components/Weather';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = "8c03acb9368aad91a16c609e75d8c405";
+
+
+export default function App() {
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function getWeatherInfo(cityName) {
+        setLoaded(false);
+        const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+        try {
+            const response = await fetch(API);
+            if(response.status == 200) {
+                const data = await response.json();
+                setWeatherData(data);
+            } else {
+                setWeatherData(null);
+            }
+            setLoaded(true);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getWeatherInfo('Toronto');
+    }, [])
+    
+
+    if(!loaded) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator color='gray'  size={36} />
+            </View>
+
+        )
+    }
+
+    else if(weatherData === null) {
+        return (
+            <View style={styles.container}>
+                <SearchBar getWeatherInfo={getWeatherInfo}/>
+                <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Weather weatherData={weatherData} getWeatherInfo={getWeatherInfo}  />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# weather_app
+#Group Project for Mobile Dev
